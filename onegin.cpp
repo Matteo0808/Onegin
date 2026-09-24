569622d8ae350525f322b79c323382efa5a672b4
Matteo0808 <robloxscreemer@gmail.com>
2026-09-25 00:23:35 +0300

    commit2

--- onegin.cpp
+++ onegin.cpp
@@ -8,21 +8,27 @@
 
 #include "QuickSort.cpp"
 
+enum ErrSuc{
+    RETURN_SUCCESS = 0,
+    RETURN_ERROR = 1
+};
+
+ErrSuc OpenInFile(const char *fileName, int *fileIn_Descriptor, FILE *fileIn, size_t* fileSize);
+ErrSuc ReadInFile(int fileIn_Descriptor, FILE *fileIn, char *Buffer, size_t fileSize);
+ErrSuc OpenOutFile(const char **arrLines, size_t *linesLen, size_t arrSize);
+void GetLens(size_t *linesLen, const char **arrLines, size_t arrSize);
 void BufferToLinesFragmentation(char *Buffer, const char **arrLines, size_t bufSize);
 size_t RunThroughBuffer(char *Buffer, size_t bufSize);
 
 int main(){
 
-    int fileIn_Descriptor = open("onegin.txt", O_RDONLY);
-    assert(fileIn_Descriptor != -1);
-    
-    FILE *fileIn = fdopen(fileIn_Descriptor, "r");
-    assert(fileIn != NULL);
-
-    struct stat fileStat = {};
-    assert(fstat(fileIn_Descriptor, &fileStat) != -1);
-
-    size_t fileSize = fileStat.st_size;
+    int fileIn_Descriptor = 0;
+    FILE *fileIn;
+    size_t fileSize = 0;
+    if (OpenInFile("Onegin.txt", &fileIn_Descriptor, fileIn, &fileSize) == RETURN_ERROR){
+        printf("Error in input file opening\n");
+        return RETURN_ERROR;
+    }
 
     char *bufferBeta = (char *)calloc(fileSize + 2, sizeof(char));
     assert(bufferBeta != NULL);
@@ -32,23 +38,22 @@ int main(){
 
     char *Buffer = bufferBeta + 1;
 
-    assert(setvbuf(fileIn, Buffer, _IONBF, fileSize) == 0);
-
-    assert(read(fileIn_Descriptor, Buffer, fileSize) > 0);
+    if (ReadInFile(fileIn_Descriptor, fileIn, Buffer, fileSize) == RETURN_ERROR){
+        printf("Error in input file reading\n");
+        return RETURN_ERROR;
+    }
 
     size_t arrSize = RunThroughBuffer(Buffer, fileSize);
+
     const char *arrLines[arrSize] = {};
     size_t arrElemSize = sizeof(arrLines[0]);
+    size_t linesLen[arrSize] = {};
 
     BufferToLinesFragmentation(Buffer, arrLines, fileSize);
 
+    GetLens(linesLen, arrLines, arrSize);
+
     printfArr(arrLines, arrSize, arrElemSize, "%s");
-
-    size_t linesLen[arrSize] = {};
-    for(int index = 0; index < arrSize; index++){
-        linesLen[index] = strlen(arrLines[index]);
-    }
-
     printfArr(linesLen, arrSize, sizeof(size_t), "%d");
 
     printf("hui1\n");
@@ -57,23 +62,60 @@ int main(){
 
     printf("hui2\n");
 
-    int fileOut_Descriptor = open("onegin_out.txt", O_CREAT | O_RDWR | O_TRUNC, 0666);
-    assert(fileOut_Descriptor != -1);
-    // if (fileOut_Descriptor == -1) {
-    //     perror("open failed");
-    //     fprintf(stderr, "errno = %d\n", errno);
-    //     return 1;
-    // }
-    printf("hui3\n");
-    for(int index = 0; index < arrSize; index++){
-        write(fileOut_Descriptor, arrLines[index], linesLen[index]);
+    int fileOut_Descriptor = 0;
+    if (OpenOutFile(arrLines, linesLen, arrSize) == RETURN_ERROR){
+        printf("Error in output file creating, opening or writing\n");
+        return RETURN_ERROR;
+    }
+    
+    free(bufferBeta);
+}
+
+ErrSuc OpenInFile(const char *fileName, int *fileIn_Descriptor, FILE *fileIn, size_t* fileSize){
+    
+    *fileIn_Descriptor = open(fileName, O_RDONLY);
+    if(*fileIn_Descriptor == -1) {return RETURN_ERROR;}
+    
+    fileIn = fdopen(*fileIn_Descriptor, "r");
+    if(fileIn == NULL) {return RETURN_ERROR;}
+
+    struct stat fileStat = {};
+    if(fstat(*fileIn_Descriptor, &fileStat) == -1) {return RETURN_ERROR;}
+
+    *fileSize = fileStat.st_size;
+    return RETURN_SUCCESS;
+}
+
+ErrSuc ReadInFile(int fileIn_Descriptor, FILE *fileIn, char *Buffer, size_t fileSize){
+    if(setvbuf(fileIn, Buffer, _IONBF, fileSize) == 0){
+        close(fileIn_Descriptor);
+        return RETURN_ERROR;
     }
 
-    printf("hui4");
-    //assert(write(fileOut_Descriptor, Buffer, fileSize) > 0);
-    free(bufferBeta);
-    close(fileOut_Descriptor);
+    if(read(fileIn_Descriptor, Buffer, fileSize) > 0){
+        close(fileIn_Descriptor);
+        return RETURN_ERROR;
+    }
+
     close(fileIn_Descriptor);
+    return RETURN_SUCCESS;
+}
+
+ErrSuc OpenOutFile(const char **arrLines, size_t *linesLen, size_t arrSize){
+    int fileOut_Descriptor = open("onegin_out.txt", O_CREAT | O_RDWR | O_TRUNC, 0666);
+    if(fileOut_Descriptor == NULL){
+        close(fileOut_Descriptor);
+        return RETURN_ERROR;
+    }
+    
+    for(int index = 0; index < arrSize; index++){
+        if (write(fileOut_Descriptor, arrLines[index], linesLen[index]) <= 0){
+            close(fileOut_Descriptor);
+            return RETURN_ERROR;
+        }
+    }
+    close(fileOut_Descriptor);
+    return RETURN_SUCCESS;
 }
 
 void BufferToLinesFragmentation(char *Buffer, const char **arrLines, size_t bufSize){
@@ -81,6 +123,12 @@ void BufferToLinesFragmentation(char *Buffer, const char **arrLines, size_t bufS
         if(Buffer[index] == '\0'){
             arrLines[index + 1] = Buffer + (index + 1);
         }
+    }
+}
+
+void GetLens(size_t *linesLen, const char **arrLines, size_t arrSize){
+    for(int index = 0; index < arrSize; index++){
+        linesLen[index] = strnlen(arrLines[index], MAXSIZE);
     }
 }
 
